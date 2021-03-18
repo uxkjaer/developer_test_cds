d834404e8ad1af377a6448a15ad5639ca633b9a7
Jakob <jakob.m.kjaer@gmail.com>
2021-03-18 11:45:33 +1000

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/Worklist.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/s4/analytics/developerTest/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.s4.analytics.developerTest.controller.Worklist", {});
+});
